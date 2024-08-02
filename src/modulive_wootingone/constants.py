8dc19d1b772f4243b6481235a0f2d1bd3e4a931e
nodemon <bernelectronic@gmail.com>
2024-08-02 17:21:01 -0400

    add constants file

---
+++ src/modulive_wootingone/constants.py
@@ -0,0 +1,33 @@
+""" . """
+
+KEYS = [
+    {"name": "1", "note": 30, "functions": ["<MODULE_INDICATOR_KEY>(A)"]},
+    {"name": "2", "note": 31, "functions": ["<SECTION_KEY>(A,0)", "<MODULE_KEY>(A,0)"]},
+    {"name": "3", "note": 32, "functions": ["<SECTION_KEY>(A,1)", "<MODULE_KEY>(A,1)"]},
+    {"name": "4", "note": 33, "functions": ["<SECTION_KEY>(A,2)", "<MODULE_KEY>(A,2)"]},
+    {"name": "5", "note": 34, "functions": ["<SECTION_KEY>(A,3)", "<MODULE_KEY>(A,3)"]},
+    {"name": "w", "note": 26, "functions": ["<SECTION_KEY>(A,4)", "<MODULE_KEY>(A,4)"]},
+    {"name": "e", "note": 8, "functions": ["<SECTION_KEY>(A,5)", "<MODULE_KEY>(A,5)"]},
+    {"name": "r", "note": 21, "functions": ["<SECTION_KEY>(A,6)", "<MODULE_KEY>(A,6)"]},
+    {"name": "t", "note": 23, "functions": ["<SECTION_KEY>(A,7)", "<MODULE_KEY>(A,7)"]},
+    {"name": "7", "note": 36, "functions": ["<SECTION_KEY>(B,0)", "<MODULE_KEY>(B,0)"]},
+    {"name": "8", "note": 37, "functions": ["<SECTION_KEY>(B,1)", "<MODULE_KEY>(B,1)"]},
+    {"name": "9", "note": 38, "functions": ["<SECTION_KEY>(B,2)", "<MODULE_KEY>(B,2)"]},
+    {"name": "0", "note": 39, "functions": ["<SECTION_KEY>(B,3)", "<MODULE_KEY>(B,3)"]},
+    {"name": "u", "note": 24, "functions": ["<SECTION_KEY>(B,4)", "<MODULE_KEY>(B,4)"]},
+    {"name": "i", "note": 12, "functions": ["<SECTION_KEY>(B,5)", "<MODULE_KEY>(B,5)"]},
+    {"name": "o", "note": 18, "functions": ["<SECTION_KEY>(B,6)", "<MODULE_KEY>(B,6)"]},
+    {"name": "p", "note": 19, "functions": ["<SECTION_KEY>(B,7)", "<MODULE_KEY>(B,7)"]},
+    {"name": "-", "note": 45, "functions": ["<MODULE_INDICATOR_KEY>(B)"]},
+    {"name": "lshift", "note": 125, "functions": ["<MODIFIER_KEY>(shift)"]},
+    {"name": "lctrl", "note": 126, "functions": ["<MODIFIER_KEY>(ctrl)"]},
+    {"name": "rctrl", "note": 127, "functions": ["<RESET_KEY>()"]},
+]
+
+
+class Animations:
+    """Type mappings to readable variables"""
+
+    DIM = 1
+    MEDIUM = 2
+    FLASHING = 3
